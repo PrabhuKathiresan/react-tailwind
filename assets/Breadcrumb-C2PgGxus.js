11c5cb44938ff14382e602f3c28653b0a0ecaa62
PrabhuKathiresan <PrabhuKathiresan@users.noreply.github.com>
2025-11-11 16:50:04 +0000

    deploy: c1a1c94eb08d8f4b747abc545cf1b82bded8bf42

---
+++ assets/Breadcrumb-C2PgGxus.js
@@ -0,0 +1,5 @@
+const e="Breadcrumb",t="",r=[{name:"items",type:"BreadcrumbItem[]",raw:"BreadcrumbItem[]",enumValues:null,required:!0,defaultValue:null,description:"List of breadcrumb items"},{name:"render",type:"(item: BreadcrumbItem) => ReactNode",raw:"(item: BreadcrumbItem) => ReactNode",enumValues:null,required:!0,defaultValue:null,description:`Rendered method for breadcrumbItem,
+This method is called for all items except the last,
+since, last item in breadcrumb will be the current page of user
+@param item BreadcrumbItem
+@returns ReactNode`}],a={name:e,description:"",props:r};export{a as default,t as description,e as name,r as props};
