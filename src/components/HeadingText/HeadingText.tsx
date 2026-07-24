00d82f7b5be8c4813028027719b587b8deec94c9
Prabhu Kathiresan <prabhukathir30@gmail.com>
2026-07-24 16:46:46 +0530

    fix: headingtext dark mode support

--- src/components/HeadingText/HeadingText.tsx
+++ src/components/HeadingText/HeadingText.tsx
@@ -3,23 +3,33 @@ import { buildClassName } from '../../utils/build-classname'
 import { HeadingTextProps } from './HeadingText.types'
 
 const Title: React.FC<HeadingTextProps> = ({ children, className }) => (
-  <h1 className={buildClassName('text-3xl font-bold', className)}>{children}</h1>
+  <h1 className={buildClassName('text-3xl font-bold text-gray-900 dark:text-white', className)}>
+    {children}
+  </h1>
 )
 
 const SubTitle: React.FC<HeadingTextProps> = ({ children, className }) => (
-  <h2 className={buildClassName('text-2xl font-semibold', className)}>{children}</h2>
+  <h2 className={buildClassName('text-2xl font-semibold text-gray-900 dark:text-white', className)}>
+    {children}
+  </h2>
 )
 
 const SubTitle2: React.FC<HeadingTextProps> = ({ children, className }) => (
-  <h3 className={buildClassName('text-xl font-semibold', className)}>{children}</h3>
+  <h3 className={buildClassName('text-xl font-semibold text-gray-900 dark:text-white', className)}>
+    {children}
+  </h3>
 )
 
 const SubTitle3: React.FC<HeadingTextProps> = ({ children, className }) => (
-  <h4 className={buildClassName('text-lg font-medium', className)}>{children}</h4>
+  <h4 className={buildClassName('text-lg font-medium text-gray-900 dark:text-white', className)}>
+    {children}
+  </h4>
 )
 
 const SubTitle4: React.FC<HeadingTextProps> = ({ children, className }) => (
-  <h5 className={buildClassName('text-base font-medium', className)}>{children}</h5>
+  <h5 className={buildClassName('text-base font-medium text-gray-900 dark:text-white', className)}>
+    {children}
+  </h5>
 )
 
 export const HeadingText = {
